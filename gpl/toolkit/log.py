d149936dcf5a49e57873198c23d4cb7e21404875
Gordon Clark <gordon.clark@botpress.com>
2023-09-30 23:21:52 +0800

    save logs to file

--- gpl/toolkit/log.py
+++ gpl/toolkit/log.py
@@ -1,6 +1,5 @@
 import logging
-from logging import StreamHandler
-
+from logging import StreamHandler, FileHandler
 
 def set_logger_format():
     root_logger = logging.getLogger()
@@ -8,6 +7,14 @@ def set_logger_format():
         fmt="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
+    # Create a file handler
+    file_handler = FileHandler('logfile.log')
+    file_handler.setFormatter(formatter)
+    
+    # Add the file handler to the logger
+    root_logger.addHandler(file_handler)
+    
+    # Update formatters for existing StreamHandlers
     for handler in root_logger.handlers:
         if isinstance(handler, StreamHandler):
             handler.setFormatter(formatter)
